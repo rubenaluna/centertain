6a384da487025c7875dd5717545459bc3e29ed57
Ruben Luna <rubenaluna@outlook.com>
2025-01-03 13:22:20 -0600

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="map-wrap">
+      <a href="https://www.maptiler.com" class="watermark"><img
+          src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
+      <div class="map" ref="mapContainer"></div>
+    </div>
+  </template>
+  
+  <script>
+  import { Map } from 'maplibre-gl';
+  import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
+  
+  export default {
+    name: "Map",
+    setup () {
+      const mapContainer = shallowRef(null);
+      const map = shallowRef(null);
+  
+      onMounted(() => {
+        const apiKey = import.meta.env.VITE_MAPTILER_API_KEY;
+  
+        const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+  
+        map.value = markRaw(new Map({
+          container: mapContainer.value,
+          style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
+          center: [initialState.lng, initialState.lat],
+          zoom: initialState.zoom
+        }));
+  
+      }),
+      onUnmounted(() => {
+        map.value?.remove();
+      })
+  
+      return {
+        map, mapContainer
+      };
+    }
+  };
+  </script>
+  
+  
+  <style scoped>
+  @import 'maplibre-gl/dist/maplibre-gl.css';
+  
+  .map-wrap {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  
+  .map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+  
+  .watermark {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+    z-index: 999;
+  }
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Home from '../components/Home.vue'
+</script>
+
+<template>
+  <main>
+    <Home/>
+  </main>
+</template>
+
+<style>
+main {
+  width: 100%;
+  height: 100vh;
+}
+</style>

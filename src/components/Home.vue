ac4e620e414bcba973d002ecd06caa8c1ccd4b38
Ruben Luna <rubenaluna@outlook.com>
2025-01-03 13:58:52 -0600

    Set the map position to the user's position

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,65 +1,85 @@
 <template>
-    <div class="map-wrap">
-      <a href="https://www.maptiler.com" class="watermark"><img
-          src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"/></a>
-      <div class="map" ref="mapContainer"></div>
-    </div>
-  </template>
-  
-  <script>
-  import { Map } from 'maplibre-gl';
-  import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
-  
-  export default {
-    name: "Map",
-    setup () {
-      const mapContainer = shallowRef(null);
-      const map = shallowRef(null);
-  
-      onMounted(() => {
-        const apiKey = import.meta.env.VITE_MAPTILER_API_KEY;
-  
-        const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
-  
-        map.value = markRaw(new Map({
-          container: mapContainer.value,
+  <div class="map-wrap">
+    <a href="https://www.maptiler.com" class="watermark"
+      ><img src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo"
+    /></a>
+    <div class="map" ref="container"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Map } from 'maplibre-gl'
+import { shallowRef, onMounted, onUnmounted, markRaw, reactive, onBeforeMount, watch } from 'vue'
+
+export default {
+  name: 'HomeMap',
+
+  setup() {
+    const container = shallowRef<HTMLElement | null>(null)
+    const map = shallowRef<Map | null>(null)
+    const position = reactive({ lng: -74.0054338, lat: 40.7124804, zoom: 14 })
+
+    onBeforeMount(() => {
+      if (navigator.geolocation != null) {
+        navigator.geolocation.getCurrentPosition((current) => {
+          position.lat = current.coords.latitude
+          position.lng = current.coords.longitude
+        })
+      }
+    })
+
+    onMounted(() => {
+      if (!container.value) {
+        return
+      }
+
+      const apiKey = import.meta.env.VITE_MAPTILER_API_KEY
+
+      map.value = markRaw(
+        new Map({
+          container: container.value,
           style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`,
-          center: [initialState.lng, initialState.lat],
-          zoom: initialState.zoom
-        }));
-  
-      }),
-      onUnmounted(() => {
-        map.value?.remove();
-      })
-  
-      return {
-        map, mapContainer
-      };
+          center: [position.lng, position.lat],
+          zoom: 15,
+        }),
+      )
+    })
+
+    watch(position, (updated) => {
+      map.value?.setCenter([updated.lng, updated.lat])
+    })
+
+    onUnmounted(() => {
+      map.value?.remove()
+    })
+
+    return {
+      map,
+      container,
     }
-  };
-  </script>
-  
-  
-  <style scoped>
-  @import 'maplibre-gl/dist/maplibre-gl.css';
-  
-  .map-wrap {
-    position: relative;
-    width: 100%;
-    height: 100%;
-  }
-  
-  .map {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
-  
-  .watermark {
-    position: absolute;
-    left: 10px;
-    bottom: 10px;
-    z-index: 999;
-  }
-  </style>
+  },
+}
+</script>
+
+<style scoped>
+@import 'maplibre-gl/dist/maplibre-gl.css';
+
+.map-wrap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.watermark {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  z-index: 999;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import Home from '../components/Home.vue'
 
 <template>
   <main>
-    <Home/>
+    <Home />
   </main>
 </template>
 
